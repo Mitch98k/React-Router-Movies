52a56f17fdf50fc93b66b32a6bf66cd94738ab3d
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2020-12-02 22:43:13 -0700

    created the routes, made them dynamic, and created links on each movie and the home button.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Link, Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -12,6 +14,8 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          console.log(response);
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -30,7 +34,10 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path='/' render={(props) => <MovieList movies={movieList} {...props}/>}/>
+        <Route path='/movies/:id' render={(props) => <Movie movie={movieList} {...props}/>}/>
+      </div>
     </div>
   );
 }
